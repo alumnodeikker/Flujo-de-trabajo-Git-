caa62657264c7bd671106066c6bc56ce201e99df
“yo” <alumnodeikker@gmail.com>
2026-03-09 11:27:51 +0100

    cargando archivo JS

---
+++ script.js
@@ -0,0 +1,17 @@
+// gestiona los precios de los productos 
+
+// ¿Qué tenemos ? = un arry let [200,150,300,120,500]
+
+//¿Qué queremos ? Eliminar el producto más pequeño "se puede hacer con un Slice" , 
+// y añadir uno nuevo al principio "de puede hacer con (unshift)", 
+// crear un nuevo array ejemplo: "miarrynuevo[]".
+
+// El resultado: podemos ver el resultado con el "Foreach", esto me va ayudar a recorrer el arry. 
+// Pieso hacerlo con una funtion que llamare GestionDeProductos. 
+
+
+
+
+
+
+
